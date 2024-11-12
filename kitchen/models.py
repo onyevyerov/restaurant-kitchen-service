8db8d3de0c81225e1f8b16dc1abbbce84c7bae37
onyevyerov <onyevyerov@gmail.com>
2024-11-12 22:44:42 +0100

    Add Ingredient model with ManyToMany relations with Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -29,6 +29,14 @@ class Dish(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField("Ingredient", related_name="dishes")
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
