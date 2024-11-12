31dc81cef828ecc4c6af8514a300291263544895
onyevyerov <onyevyerov@gmail.com>
2024-11-12 20:44:00 +0100

    Install black and changed single quotes to double

--- kitchen_service/wsgi.py
+++ kitchen_service/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kitchen service.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kitchen service.settings")
 
 application = get_wsgi_application()
