c037f7174e2a28c123d17665aecaffefe6df8233
onyevyerov <onyevyerov@gmail.com>
2024-11-12 22:04:10 +0100

    Add Cook model as User and overwrite str method

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -9,3 +10,14 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
