d5dd4225c67f827b9255768917068d544048b2d5
onyevyerov <onyevyerov@gmail.com>
2024-11-14 15:16:14 +0100

    Create path /dishes and DishListView

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("/dishes", DishListView.as_view(), name="dishes"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, Cook, DishType, Ingredient
 
@@ -17,3 +18,7 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_of_ingredients": num_of_ingredients,
     }
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishListView(generic.ListView):
+    model = Dish
