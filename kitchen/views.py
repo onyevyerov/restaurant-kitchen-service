5958ba741ce3be35f026f0d712cdefe227791000
onyevyerov <onyevyerov@gmail.com>
2024-11-14 13:03:54 +0100

    Create homepage path and view index

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, Cook, DishType, Ingredient
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_of_dishes = Dish.objects.count()
+    num_of_cooks = Cook.objects.count()
+    num_of_dish_types = DishType.objects.count()
+    num_of_ingredients = Ingredient.objects.count()
+
+    context = {
+        "num_of_dishes": num_of_dishes,
+        "num_of_cooks": num_of_cooks,
+        "num_of_dish_types": num_of_dish_types,
+        "num_of_ingredients": num_of_ingredients,
+    }
+    return render(request, "kitchen/index.html", context=context)
+
