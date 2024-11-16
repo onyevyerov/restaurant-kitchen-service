7b4ce738f2e37958d82ce66855402a65dc08affa
onyevyerov <onyevyerov@gmail.com>
2024-11-16 22:44:11 +0100

    Made Ingredient name unique

--- kitchen/models.py
+++ kitchen/models.py
@@ -36,7 +36,7 @@ class Dish(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
